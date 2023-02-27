b5e4c61ca3bbbe15d5f6d6e9cc84541109f92ede
Natalia Méndez <98635809+naty245@users.noreply.github.com>
2023-02-27 18:49:54 -0300

    agregué los archivos al repo

---
+++ js/function.js
@@ -0,0 +1,20 @@
+
+const $mostrar = document.querySelector("#icono-evento1")
+$mostrar.addEventListener("click",(e)=>{
+    document.getElementById("texto-oculto1").style.display="block";
+})
+
+
+const $mostrar1 = document.querySelector("#icono-evento2")
+$mostrar1.addEventListener("click",(e)=>{
+    document.getElementById("texto-oculto2").style.display="block";
+})
+
+
+const $mostrar2 = document.querySelector("#icono-evento3")
+$mostrar2.addEventListener("click",(e)=>{
+    document.getElementById("texto-oculto3").style.display="block";
+})
+
+
+
